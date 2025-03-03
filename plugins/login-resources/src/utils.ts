hcengineering/platform

UBERF-9537: Fix Invalid navigate to guest not authorised (#8121)
Signed-off-by: Andrey Sobolev <haiodo@gmail.com>

plugins/login-resources/src/utils.ts
@@ -31,6 +31,7 @@ import presentation, { isAdminUser } from '@hcengineering/presentation'
 import {
   fetchMetadataLocalStorage,
   getCurrentLocation,
+  isSameSegments,
   locationStorageKeyId,
   locationToUrl,
   navigate,
@@ -409,6 +410,20 @@ export async function getAccount (doNavigate: boolean = true): Promise<LoginInfo
     }
     return result.result
   } catch (err: any) {
+    if (err instanceof PlatformError && err.status.code === platform.status.Unauthorized) {
+      setMetadata(presentation.metadata.Token, null)
+      setMetadata(presentation.metadata.Workspace, null)
+      setMetadata(presentation.metadata.WorkspaceId, null)
+      setMetadataLocalStorage(login.metadata.LastToken, null)
+      setMetadataLocalStorage(login.metadata.LoginEndpoint, null)
+      setMetadataLocalStorage(login.metadata.LoginEmail, null)
+
+      const loc = getCurrentLocation()
+      loc.path[1] = 'login'
+      loc.path.length = 2
+      navigate(loc)
+      return
+    }
     Analytics.handleError(err)
   }
 }
@@ -658,11 +673,18 @@ export function navigateToWorkspace (
       // Json parse error could be ignored
     }
   }
-  const last = localStorage.getItem(`${locationStorageKeyId}_${workspace}`)
-  if (last !== null) {
-    navigate(JSON.parse(last), replace)
+  const newLoc: Location = { path: [workbenchId, workspace] }
+  let last: Location | undefined
+  try {
+    last = JSON.parse(localStorage.getItem(`${locationStorageKeyId}_${workspace}`) ?? '')
+  } catch (err: any) {
+    // Ignore
+  }
+  if (last != null && isSameSegments(last, newLoc, 2)) {
+    // If last location in our workspace path, use it.
+    navigate(last, replace)
   } else {
-    navigate({ path: [workbenchId, workspace] }, replace)
+    navigate(newLoc, replace)
   }
 }
 

plugins/workbench-resources/src/components/SelectWorkspaceMenu.svelte
@@ -28,6 +28,7 @@
     closePopup,
     fetchMetadataLocalStorage,
     getCurrentLocation,
+    isSameSegments,
     locationStorageKeyId,
     locationToUrl,
     navigate,
@@ -57,11 +58,19 @@
       e.preventDefault()
       closePopup()
       closePopup()
-      if (ws !== getCurrentLocation().path[1]) {
-        const last = localStorage.getItem(`${locationStorageKeyId}_${ws}`)
-        if (last !== null) {
-          navigate(JSON.parse(last))
-        } else navigate({ path: [workbenchId, ws] })
+      const current = getCurrentLocation()
+      if (ws !== current.path[1]) {
+        let last: Location | undefined
+        try {
+          last = JSON.parse(localStorage.getItem(`${locationStorageKeyId}_${ws}`) ?? '')
+        } catch (err: any) {
+          // Ignore
+        }
+        if (last != null && isSameSegments(last, current, 2)) {
+          navigate(last)
+        } else {
+          navigate({ path: [workbenchId, ws] })
+        }
       }
     }
   }
@@ -147,7 +156,7 @@
         {/if}
       </div>
       <div class="p-2 ml-2 mb-4 select-text flex-col bordered">
-        {decodeTokenPayload(getMetadata(presentation.metadata.Token) ?? '').workspace}
+        {decodeTokenPayload(getMetadata(presentation.metadata.Token) ?? '').workspace ?? ''}
       </div>
     {/if}
     <div class="ap-scroll">

hanzo-apps/app

plugins/login-resources/src/utils.ts
@@ -41,6 +41,7 @@ import platform, {
 import presentation from '@hcengineering/presentation'
 import {
   getCurrentLocation,
+  isSameSegments,
   locationStorageKeyId,
   locationToUrl,
   navigate,
@@ -297,6 +298,21 @@ export async function getAccount (doNavigate: boolean = true): Promise<LoginInfo
     if (err instanceof PlatformError) {
       await handleStatusError('Get account error', err.status)
 
+      if (err.status.code === platform.status.Unauthorized) {
+        setMetadata(presentation.metadata.Token, null)
+        setMetadata(presentation.metadata.Workspace, null)
+        setMetadata(presentation.metadata.WorkspaceId, null)
+        setMetadataLocalStorage(login.metadata.LastToken, null)
+        setMetadataLocalStorage(login.metadata.LoginEndpoint, null)
+        setMetadataLocalStorage(login.metadata.LoginEmail, null)
+  
+        const loc = getCurrentLocation()
+        loc.path[1] = 'login'
+        loc.path.length = 2
+        navigate(loc)
+        return
+      }
+
       return null
     } else {
       Analytics.handleError(err)
@@ -454,11 +470,18 @@ export function navigateToWorkspace (
       // Json parse error could be ignored
     }
   }
-  const last = localStorage.getItem(`${locationStorageKeyId}_${workspaceUrl}`)
-  if (last !== null) {
-    navigate(JSON.parse(last), replace)
+  const newLoc: Location = { path: [workbenchId, workspaceUrl] }
+  let last: Location | undefined
+  try {
+    last = JSON.parse(localStorage.getItem(`${locationStorageKeyId}_${workspace}`) ?? '')
+  } catch (err: any) {
+    // Ignore
+  }
+  if (last != null && isSameSegments(last, newLoc, 2)) {
+    // If last location in our workspace path, use it.
+    navigate(last, replace)
   } else {
-    navigate({ path: [workbenchId, workspaceUrl] }, replace)
+    navigate(newLoc, replace)
   }
 }
 

plugins/workbench-resources/src/components/SelectWorkspaceMenu.svelte
@@ -28,6 +28,7 @@
     closePopup,
     fetchMetadataLocalStorage,
     getCurrentLocation,
+    isSameSegments,
     locationStorageKeyId,
     locationToUrl,
     navigate,
@@ -58,11 +59,19 @@
       e.preventDefault()
       closePopup()
       closePopup()
-      if (wsUrl !== getCurrentLocation().path[1]) {
-        const last = localStorage.getItem(`${locationStorageKeyId}_${wsUrl}`)
-        if (last !== null) {
-          navigate(JSON.parse(last))
-        } else navigate({ path: [workbenchId, wsUrl] })
+      const current = getCurrentLocation()
+      if (wsUrl !== current.path[1]) {
+        let last: Location | undefined
+        try {
+          last = JSON.parse(localStorage.getItem(`${locationStorageKeyId}_${wsUrl}`) ?? '')
+        } catch (err: any) {
+          // Ignore
+        }
+        if (last != null && isSameSegments(last, current, 2)) {
+          navigate(last)
+        } else {
+          navigate({ path: [workbenchId, wsUrl] })
+        }
       }
     }
   }
@@ -148,7 +157,7 @@
         {/if}
       </div>
       <div class="p-2 ml-2 mb-4 select-text flex-col bordered">
-        {decodeTokenPayload(getMetadata(presentation.metadata.Token) ?? '').workspace}
+        {decodeTokenPayload(getMetadata(presentation.metadata.Token) ?? '').workspace ?? ''}
       </div>
     {/if}
     <div class="ap-scroll">